74d2d4be7f2bc5f5bfa8ef9971fe0328562c2d7d
Alyssa Goins <alyssa.goins@galatea-associates.com>
2025-01-23 11:42:14 -0500

    Remove unnecessary python welcome script.

--- main.py
+++ main.py
@@ -1,17 +1,6 @@
 from processor import process
 import argparse
 
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
 """ Return URL from parsed command line arguments. """
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -23,6 +12,3 @@ def parse_args():
 if __name__ == '__main__':
     url = parse_args()
     process(url)
-
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
