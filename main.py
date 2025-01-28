663daf721a3019ee41f4555e4403a8902b51b640
Alyssa Goins <alyssa.goins@galatea-associates.com>
2025-01-28 14:15:03 -0500

    update from static to object-oriented approach.

--- main.py
+++ main.py
@@ -1,14 +1,8 @@
-from processor import process
+from processor import Processor
+from retriever import Retriever
+from cleaner import Cleaner
 import argparse
-from cleaner import set_src_lang, set_target_lang
 
-
-# allowed source languages
-# todo expand out
-src_lang_options = ['DE']
-# allowed target languages
-# todo expand out
-target_lang_options = ['EN']
 
 """ Return URL from parsed command line arguments. """
 def parse_args():
@@ -16,10 +10,10 @@ def parse_args():
     parser.add_argument('--url', type=str, required=True,
                         help='Page to scrape')
     parser.add_argument('--src_lang', type=str,
-                        choices=src_lang_options,
+                        choices=['DE'],
                         help='Original page language')
     parser.add_argument('--target_lang', type=str,
-                        choices=target_lang_options,
+                        choices=['EN'],
                         help='Language to translate page')
     input_args = parser.parse_args()
     return input_args
@@ -28,12 +22,15 @@ def parse_args():
 if __name__ == '__main__':
     args = parse_args()
     url = args.url
+    src_lang = 'DE'
+    target_lang = 'EN'
 
     # use target lang arg if specified. otherwise, default 'EN' used.
     if args.target_lang is not None:
-        set_target_lang(args.target_lang)
+        src_lang = args.target_lang
     # use src lang arg if specified. otherwise, default 'DE' used.
     if args.src_lang is not None:
-        set_src_lang(args.src_lang)
+        target_lang = args.src_lang
     # process input url
-    process(url)
+    processor = Processor(Retriever(url), Cleaner(src_lang, target_lang))
+    processor.process()
--- retriever.py
+++ retriever.py
@@ -1,18 +1,23 @@
 from bs4 import BeautifulSoup
 import requests as req
 
-""" Scrapes data from input webpage and returns the body text in the form of a 
-      string list. """
-def scrape_body_text(url):
-  try:
-    response = req.get(url)
+class Retriever:
 
-    # create BeautifulSoup object with html parser
-    soup = BeautifulSoup(response.text, 'html.parser')
-    # remove tags
-    for data in soup(['style', 'script']):
-      data.decompose()
-    return soup
-  except Exception as exc:
-    raise Exception(exc)
+  def __init__(self, url):
+    self.url = url
+
+  """ Scrapes data from instance webpage and returns the body text in the form 
+    of a string list. """
+  def scrape_body_text(self):
+    try:
+      response = req.get(self.url)
+
+      # create BeautifulSoup object with html parser
+      soup = BeautifulSoup(response.text, 'html.parser')
+      # remove tags
+      for data in soup(['style', 'script']):
+        data.decompose()
+      return soup
+    except Exception as exc:
+      raise Exception(exc)
 
